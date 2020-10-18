c6e852658b52e2a72796b43ddbb2d0bc62f4d6bf
victor77dev <victor77.dev@gmail.com>
2020-10-18 10:42:52 +0200

    Add downloadJson

--- badminton/score.js
+++ badminton/score.js
@@ -1,2 +1,11 @@
+
+const downloadJson() {
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(storageObj));
+    var dlAnchorElem = document.getElementById('downloadAnchorElem');
+    dlAnchorElem.setAttribute("href",     dataStr     );
+    dlAnchorElem.setAttribute("download", "scene.json");
+    dlAnchorElem.click();
+}
+
 export const Score = {
 };
